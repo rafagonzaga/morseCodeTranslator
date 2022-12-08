6351b515c03fa30b8e2c908ee2dac86c3bc0f2b3
Rafael Gonzaga <ralozaga@gmail.com>
2022-12-07 22:41:05 -0300

    Remoção de comentários

--- src/desafioCodigoMorse/Main.java
+++ src/desafioCodigoMorse/Main.java
@@ -3,21 +3,25 @@ package desafioCodigoMorse;
 import java.util.Scanner;
 
 class Main {
+
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
         System.out.print("Digite a mensagem: ");
         String message = entrada.nextLine();
-//        String message = "Boa Sorte";
+
         Portuguese pt = new Portuguese();
         Morse mo = new Morse();
-//        String codificada = "-... --- .- / ... --- .-. - . ";
-        String codificada = ".--- --- .- --- / -.-. --- -. - .- / -.-. --- -- / .- / ... ..- .- / .- .--- ..- -.. .-";
+
+        String ajudaJoao = ".--- --- .- --- / -.-. --- -. - .- / -.-. --- -- / .- / ... ..- .- / .- .--- ..- -.. .-";
 
         String texto = pt.convertToMorse(message);
         System.out.println(texto);
-        texto = mo.convertToPortugues(texto);
-        System.out.println(texto);
+
+        ajudaJoao = mo.convertToPortugues(ajudaJoao);
+        System.out.println("\nMensagem codificada do João");
+        System.out.println(ajudaJoao);
+
         entrada.close();
     }
 }
